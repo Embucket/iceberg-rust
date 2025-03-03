1a56149ef022de5cfeb49d84ff29d5015a8886f4
Scott Lyons <scottalyons@gmail.com>
2025-03-03 00:50:33 -0800

    Making some modules public

--- datafusion_iceberg/src/lib.rs
+++ datafusion_iceberg/src/lib.rs
@@ -2,9 +2,9 @@ pub mod catalog;
 pub mod error;
 pub mod materialized_view;
 pub mod planner;
-mod pruning_statistics;
+pub mod pruning_statistics;
 pub mod sql;
-mod statistics;
+pub mod statistics;
 pub mod table;
 
 pub use crate::table::DataFusionTable;
