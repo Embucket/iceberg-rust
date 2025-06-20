75482f875548df819d4ef49ba1640729981bedd7
Jan Kaul <jan@embucket.com>
2025-06-20 11:20:43 +0200

    change visibility to pub

--- datafusion_iceberg/src/pruning_statistics.rs
+++ datafusion_iceberg/src/pruning_statistics.rs
@@ -44,13 +44,13 @@ use iceberg_rust::{
     },
 };
 
-pub(crate) struct PruneManifests<'table, 'manifests> {
+pub struct PruneManifests<'table, 'manifests> {
     partition_fields: &'table [BoundPartitionField<'table>],
     files: &'manifests [ManifestListEntry],
 }
 
 impl<'table, 'manifests> PruneManifests<'table, 'manifests> {
-    pub(crate) fn new(
+    pub fn new(
         partition_fields: &'table [BoundPartitionField<'table>],
         files: &'manifests [ManifestListEntry],
     ) -> Self {
@@ -141,14 +141,14 @@ impl PruningStatistics for PruneManifests<'_, '_> {
     }
 }
 
-pub(crate) struct PruneDataFiles<'table, 'manifests> {
+pub struct PruneDataFiles<'table, 'manifests> {
     schema: &'table Schema,
     arrow_schema: &'table ArrowSchema,
     files: &'manifests [ManifestEntry],
 }
 
 impl<'table, 'manifests> PruneDataFiles<'table, 'manifests> {
-    pub(crate) fn new(
+    pub fn new(
         schema: &'table Schema,
         arrow_schema: &'table ArrowSchema,
         files: &'manifests [ManifestEntry],
@@ -282,7 +282,7 @@ fn any_iter_to_array(
     }
 }
 
-pub(crate) fn transform_predicate(
+pub fn transform_predicate(
     expr: Expr,
     partition_fields: &[BoundPartitionField],
 ) -> Result<Expr, DataFusionError> {
