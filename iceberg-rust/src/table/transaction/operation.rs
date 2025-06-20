65a22d02478359bd47bcc96ff223bfdcee58c8e3
Jan Kaul <jan@embucket.com>
2025-06-20 11:42:15 +0200

    make snapshot_id optional for TableRequirement

--- iceberg-rust/src/table/transaction/operation.rs
+++ iceberg-rust/src/table/transaction/operation.rs
@@ -411,7 +411,7 @@ impl Operation {
                 Ok((
                     old_snapshot.map(|x| TableRequirement::AssertRefSnapshotId {
                         r#ref: branch.clone().unwrap_or("main".to_owned()),
-                        snapshot_id: *x.snapshot_id(),
+                        snapshot_id: Some(*x.snapshot_id()),
                     }),
                     vec![
                         TableUpdate::AddSnapshot { snapshot },
@@ -565,7 +565,7 @@ impl Operation {
                 Ok((
                     old_snapshot.map(|x| TableRequirement::AssertRefSnapshotId {
                         r#ref: branch.clone().unwrap_or("main".to_owned()),
-                        snapshot_id: *x.snapshot_id(),
+                        snapshot_id: Some(*x.snapshot_id()),
                     }),
                     vec![
                         TableUpdate::AddSnapshot { snapshot },
@@ -716,7 +716,7 @@ impl Operation {
                 Ok((
                     Some(TableRequirement::AssertRefSnapshotId {
                         r#ref: branch.clone().unwrap_or("main".to_owned()),
-                        snapshot_id: *old_snapshot.snapshot_id(),
+                        snapshot_id: Some(*old_snapshot.snapshot_id()),
                     }),
                     vec![
                         TableUpdate::AddSnapshot { snapshot },
@@ -753,7 +753,7 @@ impl Operation {
                         .get(&key)
                         .map(|x| TableRequirement::AssertRefSnapshotId {
                             r#ref: key.clone(),
-                            snapshot_id: x.snapshot_id,
+                            snapshot_id: Some(x.snapshot_id),
                         }),
                     vec![TableUpdate::SetSnapshotRef {
                         ref_name: key,
