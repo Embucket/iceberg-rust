af0532380770f55f23c783e555b4674cec743174
Artem Osipov <59066880+osipovartem@users.noreply.github.com>
2026-01-06 17:39:30 +0300

    Remove files with delted status when preparing manifest from existing (#53)
    * Fix double merge use case
    
    * Fix double merge use case
    
    * Fix double merge use case

--- iceberg-rust/src/table/manifest.rs
+++ iceberg-rust/src/table/manifest.rs
@@ -403,22 +403,23 @@ impl<'schema, 'metadata> ManifestWriter<'schema, 'metadata> {
             },
         )?;
 
-        writer.extend(
-            manifest_reader
-                .map(|entry| {
-                    let mut entry = entry
-                        .map_err(|err| apache_avro::Error::DeserializeValue(err.to_string()))?;
-                    *entry.status_mut() = Status::Existing;
-                    if entry.sequence_number().is_none() {
-                        *entry.sequence_number_mut() = Some(manifest.sequence_number);
-                    }
-                    if entry.snapshot_id().is_none() {
-                        *entry.snapshot_id_mut() = Some(manifest.added_snapshot_id);
-                    }
-                    to_value(entry)
-                })
-                .filter_map(Result::ok),
-        )?;
+        writer.extend(manifest_reader.filter_map(|entry| {
+            let mut entry = entry
+                .map_err(|err| apache_avro::Error::DeserializeValue(err.to_string()))
+                .ok()?;
+
+            if *entry.status() == Status::Deleted {
+                return None;
+            }
+            *entry.status_mut() = Status::Existing;
+            if entry.sequence_number().is_none() {
+                *entry.sequence_number_mut() = Some(manifest.sequence_number);
+            }
+            if entry.snapshot_id().is_none() {
+                *entry.snapshot_id_mut() = Some(manifest.added_snapshot_id);
+            }
+            to_value(entry).ok()
+        }))?;
 
         manifest.sequence_number = table_metadata.last_sequence_number + 1;
 
@@ -431,6 +432,8 @@ impl<'schema, 'metadata> ManifestWriter<'schema, 'metadata> {
 
         manifest.added_files_count = None;
         manifest.added_rows_count = None;
+        manifest.deleted_rows_count = None;
+        manifest.deleted_files_count = None;
 
         Ok(ManifestWriter {
             manifest,
@@ -545,6 +548,10 @@ impl<'schema, 'metadata> ManifestWriter<'schema, 'metadata> {
                 .map_err(|err| apache_avro::Error::DeserializeValue(err.to_string()))
                 .unwrap();
 
+            if *entry.status() == Status::Deleted {
+                return None;
+            }
+
             if entry.sequence_number().is_none() {
                 *entry.sequence_number_mut() = Some(manifest.sequence_number);
             }
@@ -580,6 +587,8 @@ impl<'schema, 'metadata> ManifestWriter<'schema, 'metadata> {
 
         manifest.added_files_count = None;
         manifest.added_rows_count = None;
+        manifest.deleted_rows_count = None;
+        manifest.deleted_files_count = None;
 
         Ok((
             ManifestWriter {
